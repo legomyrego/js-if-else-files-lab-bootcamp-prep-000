845c826be28483c8f6263f23a9882768084a8307
Mark Rego <rego121@gmail.com>
2019-07-11 16:41:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Susan"){
+  console.log("the name is correct")
+} else {
+  console.log("expected the name to be susan")
+}
+
 
 // Don't worry about this
 module.exports = { name, height
 }
-

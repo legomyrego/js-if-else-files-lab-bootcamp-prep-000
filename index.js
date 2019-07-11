d69eb7ac073183dc7ce15ad71b87375063b3b19c
Mark Rego <rego121@gmail.com>
2019-07-11 17:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 var name = "Susan"
 var height = "70"
 
-if (name === "Susan"){
-  console.log("the name is correct")
-} else {
-  console.log("expected the name to be susan")
-}
 
-if (height === '70'){
-  console.log('The height is correct')
-} else {
-  console.log('expected the height to be 70')
-}
 
 
 // Don't worry about this
